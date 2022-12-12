aa4c21a0f61c7263b274f89fc6203f8b210bf259
normanagudelo <normanagudelo@gmail.com>
2022-12-12 18:04:10 -0500

    V2
    Revision completa

--- signin.js
+++ signin.js
@@ -1,10 +1,10 @@
 var basedatos=[]
 
 var guardarusuario= function(){
-    var name=document.getElementById('nombreid').value
+
     var email=document.getElementById('correoid').value
     var password=document.getElementById('passwordid').value
-    basedatos.push({nombre:name,mail:email,pass:password})
+    basedatos.push({email,pass:password})
     localStorage.setItem("base", JSON.stringify(basedatos))
 
 }
